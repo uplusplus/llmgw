1536135cec6fd1b06ff8e7d0e8a5ee21b3af6d00
uplusplus <yjcpui@gmail.com>
2026-04-13 20:41:30 +0800

    fix cdp connect timeout issue.

--- install.ps1
+++ install.ps1
@@ -265,7 +265,7 @@ if ($chromePath) {
         for ($i = 1; $i -le 15; $i++) {
             Start-Sleep -Seconds 1
             try {
-                $resp = Invoke-WebRequest -Uri "http://localhost:$CDP_PORT/json/version" -UseBasicParsing -TimeoutSec 2 -ErrorAction Stop
+                $resp = Invoke-WebRequest -Uri "http://localhost:$CDP_PORT/json/version" -UseBasicParsing -TimeoutSec 20 -ErrorAction Stop
                 $content = $resp.Content
                 if ($content -match '"Browser"') {
                     $ready = $true
